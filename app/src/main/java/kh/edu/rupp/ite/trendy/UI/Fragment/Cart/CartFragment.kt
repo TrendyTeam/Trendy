5fa4d6437f689b682bb8e7868e055058be7607f8
RathaMnusSmos <dalyloy13@gmail.com>
2023-10-27 16:06:38 +0700

    home module update

--- app/src/main/java/kh/edu/rupp/ite/trendy/UI/Fragment/Cart/CartFragment.kt
+++ app/src/main/java/kh/edu/rupp/ite/trendy/UI/Fragment/Cart/CartFragment.kt
@@ -1,12 +1,6 @@
 package kh.edu.rupp.ite.trendy.UI.Fragment.Cart
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import kh.edu.rupp.ite.trendy.Base.BaseFragmentBinding
-import kh.edu.rupp.ite.trendy.R
 import kh.edu.rupp.ite.trendy.databinding.FragmentCartBinding
 
 
--- app/src/main/java/kh/edu/rupp/ite/trendy/UI/Main/MainActivity.kt
+++ app/src/main/java/kh/edu/rupp/ite/trendy/UI/Main/MainActivity.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import kh.edu.rupp.ite.trendy.Base.BaseActivityBinding
 import kh.edu.rupp.ite.trendy.R
 import kh.edu.rupp.ite.trendy.UI.Fragment.Cart.CartFragment
+import kh.edu.rupp.ite.trendy.UI.Fragment.Favorite.FavoriteFragment
 import kh.edu.rupp.ite.trendy.UI.Fragment.Home.HomeFragment
+import kh.edu.rupp.ite.trendy.UI.Fragment.Profile.ProfileFragment
+import kh.edu.rupp.ite.trendy.UI.Fragment.Shop.ShopFragment
 import kh.edu.rupp.ite.trendy.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivityBinding<ActivityMainBinding>() {
+    private var STATE = 0
     override fun getLayoutViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
     override fun initView() {
         setStatusBarColor(ActivityCompat.getColor(this, R.color.indicator))
-
         initBottomNavigate()
 
     }
@@ -23,10 +26,27 @@ class MainActivity : BaseActivityBinding<ActivityMainBinding>() {
             when(item.itemId){
                 R.id.home ->{
                     showFragment(HomeFragment())
+                    STATE = 0
+                    true
+                }
+                R.id.shop -> {
+                    showFragment(ShopFragment())
+                    STATE = 1
                     true
                 }
                 R.id.cart ->{
                     showFragment(CartFragment())
+                    STATE = 2
+                    true
+                }
+                R.id.favorite ->{
+                    showFragment(FavoriteFragment())
+                    STATE = 3
+                    true
+                }
+                R.id.profile ->{
+                    showFragment(ProfileFragment())
+                    STATE = 4
                     true
                 }
                 else ->{
@@ -46,6 +66,26 @@ class MainActivity : BaseActivityBinding<ActivityMainBinding>() {
     }
 
 
+    override fun onResume() {
+        super.onResume()
 
+        when(STATE){
+            0 ->{
+                binding.bottomNavigation.selectedItemId = R.id.home
+            }
+            1 -> {
+                binding.bottomNavigation.selectedItemId = R.id.shop
+            }
+            2 -> {
+                binding.bottomNavigation.selectedItemId = R.id.cart
+            }
+            3 -> {
+                binding.bottomNavigation.selectedItemId = R.id.favorite
+            }
+            4 -> {
+                binding.bottomNavigation.selectedItemId = R.id.profile
+            }
+        }
+    }
 
 }
