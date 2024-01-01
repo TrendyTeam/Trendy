b3dd9f8ea65356f41bce3f2dff6a3abda0a8abd8
RathaMnusSmos <dalyloy13@gmail.com>
2024-01-01 21:39:26 +0700

    update app bar

--- app/src/main/java/kh/edu/rupp/ite/trendy/UI/Fragment/Shop/ShopFragment.kt
+++ app/src/main/java/kh/edu/rupp/ite/trendy/UI/Fragment/Shop/ShopFragment.kt
@@ -3,8 +3,10 @@ package kh.edu.rupp.ite.trendy.UI.Fragment.Shop
 import kh.edu.rupp.ite.trendy.Base.BaseFragmentBinding
 import kh.edu.rupp.ite.trendy.databinding.FragmentShopBinding
 
+
 class ShopFragment : BaseFragmentBinding<FragmentShopBinding>() {
     override fun getViewBinding(): FragmentShopBinding = FragmentShopBinding.inflate(layoutInflater)
+
     override fun onViewCreated() {
 
     }
