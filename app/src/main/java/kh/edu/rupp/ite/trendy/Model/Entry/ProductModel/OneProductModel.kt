e92d8c0bafdecc7d265d653dce716c39ab4e06b0
Sovanroth13 <sovanroth016@gmail.com>
2024-01-08 23:22:14 +0700

    kotlin api file for one product

---
+++ app/src/main/java/kh/edu/rupp/ite/trendy/Model/Entry/ProductModel/OneProductModel.kt
@@ -0,0 +1,82 @@
+package kh.edu.rupp.ite.trendy.Model.Entry.ProductModel
+
+
+import com.google.gson.annotations.SerializedName
+
+data class OneProductModel(
+    @SerializedName("amount")
+    var amount: Int?,
+    @SerializedName("available_color")
+    var availableColor: List<AvailableColor?>?,
+    @SerializedName("available_size")
+    var availableSize: List<AvailableSize?>?,
+    @SerializedName("category_id")
+    var categoryId: Int?,
+    @SerializedName("id")
+    var id: Int?,
+    @SerializedName("image")
+    var image: List<Image?>?,
+    @SerializedName("items")
+    var items: List<Item?>?,
+    @SerializedName("product_discount")
+    var productDiscount: Int?,
+    @SerializedName("product_name")
+    var productName: String?,
+    @SerializedName("product_price")
+    var productPrice: Double?
+) {
+    data class AvailableColor(
+        @SerializedName("amount")
+        var amount: Int?,
+        @SerializedName("color")
+        var color: String?,
+        @SerializedName("color_code")
+        var colorCode: String?
+    )
+
+    data class AvailableSize(
+        @SerializedName("amount")
+        var amount: Int?,
+        @SerializedName("color_onSize")
+        var colorOnSize: List<ColorOnSize?>?,
+        @SerializedName("size")
+        var size: String?
+    ) {
+        data class ColorOnSize(
+            @SerializedName("amount")
+            var amount: Int?,
+            @SerializedName("color")
+            var color: String?,
+            @SerializedName("color_code")
+            var colorCode: String?
+        )
+    }
+
+    data class Image(
+        @SerializedName("color")
+        var color: String?,
+        @SerializedName("color_code")
+        var colorCode: String?,
+        @SerializedName("image_id")
+        var imageId: Int?,
+        @SerializedName("image_onColor")
+        var imageOnColor: String?,
+        @SerializedName("image_url")
+        var imageUrl: String?,
+        @SerializedName("public_id")
+        var publicId: String?
+    )
+
+    data class Item(
+        @SerializedName("amount")
+        var amount: Int?,
+        @SerializedName("color")
+        var color: String?,
+        @SerializedName("color_code")
+        var colorCode: String?,
+        @SerializedName("item_id")
+        var itemId: Int?,
+        @SerializedName("size")
+        var size: String?
+    )
+}
