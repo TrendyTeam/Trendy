5a69684b333d4d6933567b7cdcf0f626998714cc
Sovanroth13 <sovanroth016@gmail.com>
2024-01-12 17:15:21 +0700

    fix error for cart data

--- app/src/main/java/kh/edu/rupp/ite/trendy/UI/ViewHolder/CartViewHolder.kt
+++ app/src/main/java/kh/edu/rupp/ite/trendy/UI/ViewHolder/CartViewHolder.kt
@@ -1,7 +1,9 @@
 package kh.edu.rupp.ite.trendy.UI.ViewHolder
 
+import android.annotation.SuppressLint
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kh.edu.rupp.ite.trendy.Model.Entry.CartModel.CartModel
 import kh.edu.rupp.ite.trendy.databinding.BagItemViewHolderBinding
 
@@ -11,7 +13,14 @@ class CartViewHolder (
 
     private  val binding: BagItemViewHolderBinding = BagItemViewHolderBinding.bind(itemView)
 
+    @SuppressLint("SetTextI18n")
     fun onBind (item: CartModel.Cart) {
         binding.productName.text = item.productName
+//        Picasso.get().load(item.image!![0]?.imageUrl).into(binding.coverImage)
+        Picasso.get().load(item.image).into(binding.cardImage)
+        binding.colorFromApi.text = item.color
+        binding.sizeFromApi.text = item.size
+        binding.priceFromApi.text = "$ ${item.productPrice}"
+
     }
 }
