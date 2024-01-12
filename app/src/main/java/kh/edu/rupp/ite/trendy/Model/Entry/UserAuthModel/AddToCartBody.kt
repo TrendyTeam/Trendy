9dff3d51f0e5ab7701ee8f8649126db92fd5e7be
RathaMnusSmos <dalyloy13@gmail.com>
2024-01-12 15:30:12 +0700

    update error data rseponse

--- app/src/main/java/kh/edu/rupp/ite/trendy/Model/Entry/UserAuthModel/AddToCartBody.kt
+++ app/src/main/java/kh/edu/rupp/ite/trendy/Model/Entry/UserAuthModel/AddToCartBody.kt
@@ -1,7 +1,7 @@
 package kh.edu.rupp.ite.trendy.Model.Entry.UserAuthModel
 
 data class AddToCartBody(
-    private var user_id: String,
+    private var user_id: Int,
     private var product_item_id: String,
     private var quantity: Int
 
